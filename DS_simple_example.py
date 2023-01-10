b063eae65ae9452d4549f90f5cd968bf0f32a09c
Ghazaleh Zehtab <ghazalze@yahoo.com>
2023-01-10 17:19:24 -0500

    list of dictionary added

--- DS_simple_example.py
+++ DS_simple_example.py
@@ -1,6 +1,12 @@
 # create a dictionary for your courses grades and print the items of it : 
 my_dictionary = {'BP' : 'A', 'AP' : 'A+' , 'DS' : 'B'}
 
-print( 'This the Dictionar', my_dictionary)
+print( 'This the Dictionary', my_dictionary)
 
 print( "This the items : " , my_dictionary.items())
+
+# Store the items of the dictionary in a list
+
+List_of_Dictionary = list(my_dictionary.items())
+
+print("list of dictionary : ", List_of_Dictionary)
