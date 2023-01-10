cfb4a0f457a12ee9e877144f2d81d21a614d2cdd
Ghazaleh Zehtab <ghazalze@yahoo.com>
2023-01-10 16:59:02 -0500

    dictionary added

---
+++ DS_simple_example.py
@@ -0,0 +1,6 @@
+# create a dictionary for your courses grades and print the items of it : 
+my_dictionary = {'BP' : 'A', 'AP' : 'A+' , 'DS' : 'B'}
+
+print( 'This the Dictionar', my_dictionary)
+
+print( "This the items : " , my_dictionary.items())
