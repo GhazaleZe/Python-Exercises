e676f85ba6d3540bf2212d040d00a787d0eaa998
Ghazaleh Zehtab <ghazalze@yahoo.com>
2023-01-28 13:51:08 -0500

    Class updated

--- OOP.py
+++ OOP.py
@@ -1,12 +1,13 @@
 import datetime
 
 
-class person:
+class Person:
     count = 0
     def __init__(self, year_of_birth, first_name, last_name):
         self.year_of_birth = year_of_birth
         self.first_name = first_name
         self.last_name = last_name
+        Person.count += 1
     def get_first_name_and_last_name(self):
         print("The first name is %s" % self.first_name)
         print("The last name is %s" % self.last_name)
@@ -16,8 +17,18 @@ class person:
         year = today.year
         self.age = year - self.year_of_birth
         print ("The age is %d" % self.age)
+    def HowManyPerson(self):
+        return Person.count
+        
         
 
-Katty = person(1998, 'Katty', 'Pron')
+Katty = Person(1998, 'Katty', 'Pron')
 Katty.get_first_name_and_last_name()
 Katty.get_age()
+
+Joel = Person(1969, 'Joel', 'Jo Jo')
+Joel.get_first_name_and_last_name()
+
+print("There is %s Person now" % Person.count)
+print("There is %s Person now" % Joel.count)
+
