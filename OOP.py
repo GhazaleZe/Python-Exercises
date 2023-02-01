61978946f3e269e478b728298839bc8258763853
Ghazaleh Zehtab <ghazalze@yahoo.com>
2023-01-31 21:01:12 -0500

    Class inheritence

--- OOP.py
+++ OOP.py
@@ -16,11 +16,18 @@ class Person:
         today = datetime.date.today()
         year = today.year
         self.age = year - self.year_of_birth
-        print ("The age is %d" % self.age)
+        print ("%s's age is %d" % (self.first_name , self.age))
     def HowManyPerson(self):
         return Person.count
-        
-        
+
+# inheritance in python     
+class Student(Person):
+    pass
+
+class Staff(Person):
+    def get_first_name_and_last_name(self):
+        print("The first name of this staff is %s" % self.first_name)
+        print("The last name of this staff is %s" % self.last_name)
 
 Katty = Person(1998, 'Katty', 'Pron')
 Katty.get_first_name_and_last_name()
@@ -32,3 +39,19 @@ Joel.get_first_name_and_last_name()
 print("There is %s Person now" % Person.count)
 print("There is %s Person now" % Joel.count)
 
+Amir = Student(2001, 'Amir', 'AAB')
+
+Amir.get_age()
+
+# size is defined by itself
+Amir.weight = 84
+
+print(Amir.weight)
+
+Katty.weight = 60
+
+print(Katty.weight)
+
+coco = Staff(1976, 'CoCo', 'Paul')
+
+coco.get_first_name_and_last_name()
